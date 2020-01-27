2d1d15051b29435ca5e2aafed8f8c55ab63af9c8
Charlie <45878178+Charlie1987@users.noreply.github.com>
2020-01-27 14:59:21 -0600

    Add files via upload

---
+++ Ink.ps1
@@ -0,0 +1,5 @@
+﻿#
+#
+#  enabling Windows Ink Workplace
+
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PenWorkspace -Name PenWorkspaceButtonDesiredVisibility -PropertyType DWord -Value 1
